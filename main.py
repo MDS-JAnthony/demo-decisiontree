1bb3bb08f65509a216984b182cf619fe24737d42
James Anthony <james.f.anthony@btinternet.com>
2021-11-03 11:12:42 +0000

    template decision tree class

---
+++ main.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    # Test decision tree out
+    # TODO: Load train/test datasets (using pandas read_csv)
+
+    # TODO: Prep data for modelling (Handle categorical data etc, missing values)
+
+    # TODO: Initialise decision tree class
+
+    # TODO: Fit model
+
+    # TODO: Test model
+    pass
---
+++ src/decisiontree.py
@@ -0,0 +1,30 @@
+# import required packages
+
+# TODO: Create decision tree class
+
+
+class DecisionTree:
+    def __init__(self, max_depth, min_sample, ):
+        pass
+
+    def fit(self):
+        pass
+
+    def fit_predict(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def _cost_function(self):
+        # Demo to use gini index, could also use logloss (cross entropy)
+        pass
+
+    def _split(self):
+        pass
+
+    def _build_tree(self):
+        pass
+
+    def get_params(self):
+        pass
